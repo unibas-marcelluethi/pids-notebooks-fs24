3bf92c8b98dcd041f25efa0abe7acb7075afe573
Marcel Luethi <marcel.luethi@unibas.ch>
2024-04-11 06:12:28 +0200

    add insurance week 5

---
+++ insurance/insurance5/tests/Question 2.py
@@ -0,0 +1,19 @@
+# +
+from otter.test_files import test_case
+from asserts import *
+
+OK_FORMAT = False
+name = "Question 2"
+points = 1
+@test_case()
+def test_subplots(env):
+    ts1 = 0.0521
+    ts2 = 0.9015
+    
+    p_xe3 = env["Question2"].p_xe3
+    p_xlt10 = env["Question2"].p_xlt10
+    assert(ts1 - 0.01 < p_xe3 < ts1 + 0.01 )
+    assert(ts2 - 0.01 < p_xlt10 < ts2 + 0.01 )
+# -
+
+
