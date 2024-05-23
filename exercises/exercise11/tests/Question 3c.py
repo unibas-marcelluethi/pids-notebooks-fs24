e433be4d57874df56f3c8ab3022c8409fd1cde95
Marcel Luethi <marcel.luethi@unibas.ch>
2024-05-23 10:47:11 +0200

    fix tests

--- exercises/exercise11/tests/Question 3c.py
+++ exercises/exercise11/tests/Question 3c.py
@@ -15,13 +15,13 @@ def test_q2a(env):
    X_test = env["Question3c"].X_test
    y_test = env["Question3c"].y_test
    
-   assert_true(520 < X_train.shape[0] < 530)
+   assert_true(700 < X_train.shape[0] < 800)
    assert_equal(X_train.shape[0], y_train.shape[0])
    
   
-   assert_true(390 < X_val.shape[0] < 400)
+   assert_true(200 < X_val.shape[0] < 300)
    assert_equal(X_val.shape[0], y_val.shape[0])
    
-   assert_true(390 < X_test.shape[0] < 400)
+   assert_true(200 < X_test.shape[0] < 300)
    assert_equal(X_test.shape[0], y_test.shape[0])
    
